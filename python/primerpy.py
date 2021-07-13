f1f7ead1475d89553fee1f7a8fd77dcf15901167
juanjoselo <mrjuanjose91@gmail.com>
2021-07-13 09:46:06 -0500

    Primer file py

---
+++ python/primerpy.py
@@ -0,0 +1,7 @@
+pesos = input ("Cuantos Pesos Tienes? ")
+pesos = float(pesos)
+valordolar = 22
+dolares = float(pesos/valordolar)
+dolares = round(dolares,2)
+dolares = str(dolares)
+print ("Tu tienes $"+ dolares + " Dolares")
